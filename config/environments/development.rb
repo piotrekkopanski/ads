4bdc5143fc9ba242c294bdbdd2a5709775df14dc
piotrkopanski <piotrkopanski@interia.pl>
2018-03-13 19:54:10 +0100

    set time zone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.time_zone = "Europe/Warsaw"
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
