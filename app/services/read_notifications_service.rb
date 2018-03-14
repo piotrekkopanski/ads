6eb9643318d78af65924c12a6a86c98c8b76a6f0
piotrkopanski <piotrkopanski@interia.pl>
2018-03-14 20:36:50 +0100

    update read after return notifications

--- app/services/read_notifications_service.rb
+++ app/services/read_notifications_service.rb
@@ -2,10 +2,10 @@ class ReadNotificationsService
 
   public
 
-  def self.read(x,hash = { })
+  def self.read(x,hash = { })  
     @notifications = Notification.where(read: false).where(hash).first(x)
+    puts @notifications.to_s
     @notifications.map{ |notification| notification.update_attributes( { read: true } ) }   
-    return @notifications
   end
 
 end
