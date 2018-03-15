f10fac98436e03fc7acacad09dd2d86856fd0143
piotrkopanski <piotrkopanski@interia.pl>
2018-03-15 09:37:58 +0100

    add the default url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'notifications#new'
+
   resources :notifications
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
