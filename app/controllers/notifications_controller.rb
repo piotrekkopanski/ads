8264ebc32b50371b494f158b9fec9c9cfa5f4814
piotrkopanski <piotrkopanski@interia.pl>
2018-03-15 19:44:54 +0100

    add read to strong parameters

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -69,6 +69,6 @@ class NotificationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
-      params.require(:notification).permit(:description, :category, :email, :cost, :term)
+      params.require(:notification).permit(:description, :category, :email, :cost, :term, :read)
     end
 end
