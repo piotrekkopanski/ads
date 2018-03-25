9e3de9effb9d24c300e8f6068bcacb447a1fd8d8
piotrkopanski <piotrkopanski@interia.pl>
2018-03-25 16:52:12 +0200

    check keys in options

--- app/services/read_notifications_service.rb
+++ app/services/read_notifications_service.rb
@@ -1,11 +1,23 @@
 class ReadNotificationsService
 
+  def self.valid_keys(options)
+    expected_keys_string = ["email", "category", "cost"]
+    expected_keys_symbol = [:email, :category, :cost]
+    if (options.keys - expected_keys_string).empty? or (options.keys - expected_keys_symbol).empty?
+      return true
+    end
+  end
+
   public
 
-  def self.read(x,hash = { })  
-    @notifications = Notification.where(read: false).where(hash).first(x)
-    puts @notifications.to_s
-    @notifications.map{ |notification| notification.update_attributes( { read: true } ) }   
+  def self.read(x, options = { } ) 
+    if valid_keys(options)
+      @notifications = Notification.where(read: false).where(options).first(x)
+      puts @notifications.to_s
+      @notifications.map{ |notification| notification.update_attributes( { read: true } ) } 
+    else
+      puts "Wrong options"     
+    end 
   end
 
 end
